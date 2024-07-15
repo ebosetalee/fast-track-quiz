a9ddf735872f546514227034fa94a61fcb1639b6
Emmanuella <princess01ella.miley@gmail.com>
2024-07-15 19:22:41 +0200

    feat: add required cli flags

--- cmd/statistics.go
+++ cmd/statistics.go
@@ -9,11 +9,11 @@ var statsCmd = &cobra.Command{
 	Use:   "stats",
 	Short: "Compare your result",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		quizCLI, err := quiz.NewCLI("http://localhost:8080")
+		quizCLI, err := quiz.NewCLI(baseURL)
 		if err != nil {
 			return err
 		}
-		err = quizCLI.Statistics("ebose")
+		err = quizCLI.Statistics(userId)
 		if err != nil {
 			return err
 		}
@@ -23,4 +23,7 @@ var statsCmd = &cobra.Command{
 
 func init() {
 	cliCmd.AddCommand(statsCmd)
+	statsCmd.Flags().StringVar(&userId, "user-id", "", "User ID")
+	statsCmd.Flags().StringVar(&baseURL, "host", "http://localhost:8080", "Base URL")
+	statsCmd.MarkFlagRequired("user-id")
 }
